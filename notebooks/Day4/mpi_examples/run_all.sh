eaa8b18aec7b71860db6cfead3a2f91eca3b509a
Carsten Bauer <crstnbr@gmail.com>
2025-08-13 15:03:54 +0200

    basic mpi

--- notebooks/Day4/mpi_examples/run_all.sh
+++ notebooks/Day4/mpi_examples/run_all.sh
@@ -1,9 +1,10 @@
 # uncomment this if you are on cluster gpu node
-# ml julia
+ml juliahpc
 # ml nvidia/nvhpc
-# ml compiler/nvidia
-# export OMPI_MCA_mpi_cuda_support=0
+ml mpi/openmpi
+export OMPI_MCA_mpi_cuda_support=0
 # export OMPI_MCA_btl_openib_warn_no_device_params_found=0
+export OMPI_MCA_mca_base_component_show_load_errors=0
 
 for f in *.jl
 do
