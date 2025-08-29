7469a681843c34082bff0e2c2b19081fe00d25e8
Carsten Bauer <crstnbr@gmail.com>
2025-08-29 08:06:36 +0200

    mpi+cuda fix/test

--- julia_wrapper.sh
+++ julia_wrapper.sh
@@ -3,13 +3,13 @@
 # Making module / ml available
 # ------------------------------------------------------------
 export MODULEPATH=/opt/system/modulefiles:/opt/training/modulefiles:/opt/hlrs/non-spack/modulefiles:/opt/hlrs/spack/current/modulefiles
+export MODULEPATH="/shared/akad-julia/modules:$MODULEPATH"
 source /opt/system/lmod/lmod/init/profile
 # ------------------------------------------------------------
 
 # Load julia
-ml julia
-ml nvidia/nvhpc # for MPI/CUDA
-ml compiler/nvidia
+ml juliahpc
+# ml mpi/openmpi # for MPI
 
 # Pass on all arguments to julia
 exec julia "${@}"
